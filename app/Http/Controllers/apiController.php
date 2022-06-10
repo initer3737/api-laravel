bd65a540503d0eee4431e71b4c9040291ce9d8b9
puthut wahyu aji <puthutwahyuaji13@gmail.com>
2022-06-10 07:01:18 +0700

    fix code for rest api

--- app/Http/Controllers/apiController.php
+++ app/Http/Controllers/apiController.php
@@ -9,30 +9,21 @@ class apiController extends Controller
 {
     #to read data from db
     public function ApiIndex()
-    {
-      try{  
+    { 
         $data=api::all();
         $headers=["status"=>"ok!"];
         return response()->json($data, 200, $headers);
-      }catch(exception $err){
-          $headers=["message"=>"error {$err}"];
-        return response()->json(["eror"=>$err], 500, $headers);
-      }
     }
     #to read data from db by id
     public function ApiIndexId($id)
-    {
-      try{  
+    { 
         $data=api::find($id);
-        // if(is_null($data)){
-        //     $headers=['status'=>'404','message'=>"data not found"];
-        //     return response()->json("not found!", 404, $headers);
-        // }
+        if(is_null($data)){
+            $headers=['status'=>'404','message'=>"data not found"];
+            return response()->json("not found!", 404, $headers);
+        }
             $headers=["status"=>"ok!"];
             return response()->json($data, 200, $headers);
-        }catch(\Exception $err){
-            return response()->json(["message"=>$err->getMessage()], 500);
-        }
     }
     #to create data 
     public function ApiCreate(Request $request)
